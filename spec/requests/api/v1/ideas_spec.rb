98b206a972fd58f1b32c3f6d473c13ac51d046e8
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-04-10 04:26:00 +0000

    アイデア管理API完成

--- spec/requests/api/v1/ideas_spec.rb
+++ spec/requests/api/v1/ideas_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe "ideas_controller", type: :request do
           get api_v1_ideas_path, params: @valid_params 
           json = JSON.parse(response.body)
           expect(json['data'][0]["category"]).to eq "category_name_hoge"
+          expect(json['data'].length).to eq(1)
         end
       end
 
@@ -33,7 +34,6 @@ RSpec.describe "ideas_controller", type: :request do
         create_list(:idea, 10)
         get api_v1_ideas_path
         json = JSON.parse(response.body)
-
         expect(response.status).to eq(200)
         expect(json['data'].length).to eq(10)
       end
@@ -46,7 +46,7 @@ RSpec.describe "ideas_controller", type: :request do
 
     context "category_nameが既存のものでない場合" do
       it "categoriesテーブルに新規追加される" do
-        valid_params = { category_name: "new_category_name", body: "body" }
+        valid_params = { category_name: "new_category_name", body: "new_body" }
         expect { post api_v1_ideas_path, params: valid_params }.to change(Category, :count).by(+1)
         .and change(Idea, :count).by(+1)
       end
@@ -71,24 +71,11 @@ RSpec.describe "ideas_controller", type: :request do
 
     context "登録に失敗した場合" do
       it "ステータスコード422が返る" do
-        valid_params = { category_name: "name" }
-        expect { post api_v1_ideas_path, params: valid_params }.to change(Idea, :count).by(0)
+        invalid_params = { category_name: "name" }
+        expect { post api_v1_ideas_path, params: invalid_params }.to change(Idea, :count).by(0)
         expect(response.status).to eq(422)
       end
     end
   end
 
 end
-
-
-# アイデア登録API
-  # category_nameが既存のものでなければcategoriesテーブルに新規追加される。
-  # category_nameが既存のものであれば、既存のcategory_idを使用する。
-  # 登録が成功したらステータスコード201が返る。
-  # 登録が失敗したらステータスコード422が返る。
-
-# アイデア取得API
-  # アイデア一覧を返却する
-  # category_nameが指定されている場合、該当するcategoryのideasの一覧を返却する。
-  # category_nameが指定されていない場合はすべてのideasを返却する。
-  # 登録されていないカテゴリーのリクエストの場合はステータスコード404を返す。
